da176ecd21ff262e0df7f18c15a41e67defd6311
michalrzak <michal.r.zak@protonmail.com>
2022-04-27 15:57:05 +0200

    docker improvements

--- server/rest_api.py
+++ server/rest_api.py
@@ -6,26 +6,35 @@ from server.response_generator import ResponseGenerator
 app = Flask(__name__)
 chatbot: ResponseGenerator = None
 
-invalid_request = {"error": "Invalid request format"}, 400
-internal_error = {"error": "Internal server error, please try again"}, 500
+IP_ADDRESS = "0.0.0.0"
+PORT = 5000
+
+INVALID_REQUEST = {"error": "Invalid request format"}, 400
+INTERNAL_ERROR = {"error": "Internal server error, please try again"}, 500
 
 
 def start_api(args, model, tokenizer):
-    global chatbot, app
+    global chatbot, app, IP_ADDRESS, PORT
     chatbot = ResponseGenerator(args, model, tokenizer)
-    app.run(debug=True, use_reloader=False, host='0.0.0.0')
+
+    from waitress import serve
+    print(f"Server started on http://{IP_ADDRESS}:{PORT}")
+    print("Press CTRL+C to stop the app")
+    serve(app, host=IP_ADDRESS, port=PORT)
+
 
 
 @app.post("/response")
 def get_response():
-    global chatbot, internal_error, invalid_request
+    global chatbot, INTERNAL_ERROR, INVALID_REQUEST
+
     if not request.is_json:
-        return invalid_request
+        return INVALID_REQUEST
 
     input_request = request.get_json()
 
     if "utterance" not in input_request or "sentiment" not in input_request:
-        return invalid_request
+        return INVALID_REQUEST
 
     utterance = input_request["utterance"]
     sentiment = Sentiment.from_string(input_request["sentiment"])
@@ -34,7 +43,7 @@ def get_response():
         response = chatbot.get_response(sentiment, utterance)
     except Exception as e:
         print(e)
-        return internal_error
+        return INTERNAL_ERROR
 
     return {"response": response}, 200
 
